0de64969a454b27d509d38c28f3dc27e01ff39b6
xcg2003 <xaviergraham54@gmail.com>
2024-10-13 14:08:05 -0700

    Refactor server code to remove unnecessary functionality

--- index.js
+++ index.js
@@ -20,50 +20,28 @@ app.use(express.urlencoded({ extended: true }));
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// array of messages
-let pageMessages = [];
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'chat.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
     res.status(200);
 });
 
 app.post('/', (req, res) => {
-    const title = req.body.title;
-    const username = req.body.username;
-    const message = req.body.message;
-    const date = new Date();
-    if(!title || !username || !message) {
-        return res.status(400).send("Username and message are required");
-    }
-    pageMessages.push({ _id: Date.now().toString(), title, username, message, date });
-    res.redirect('/blogpost');
+
 });
 
 app.get("/blogpost", (req, res) => {
-    res.render('displayMessage', { messages: pageMessages });
+    
 });
 
 // Delete post path
 app.delete('/post/:id', (req, res) => {
-    const postId = req.params.id;
-    pageMessages = pageMessages.filter(msg => msg._id !== postId);
-    res.status(200).json({ success: true });
+
 });
 
 // Edit post path
 app.put('/post/:id', (req, res) => {
-    const postId = req.params.id;
-    const { title, username, message } = req.body;
-    const post = pageMessages.find(msg => msg._id === postId);
-    if (post) {
-        post.title = title;
-        post.username = username;
-        post.message = message;
-        res.status(200).json({ success: true });
-    } else {
-        res.status(404).send("Post not found");
-    }
+  
 });
 
 
