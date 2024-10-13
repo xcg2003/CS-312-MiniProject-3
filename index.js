aab1b251716393822de7aa1c2565b0aff140cdc7
xcg2003 <xaviergraham54@gmail.com>
2024-10-13 14:12:07 -0700

    Node packages change

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
