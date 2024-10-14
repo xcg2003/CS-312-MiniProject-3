5aaa3edc9c4fee24ca6a00b4e92f9c3e767f94c9
Xavier <128425706+xcg2003@users.noreply.github.com>
2024-10-14 10:45:11 -0700

    Added more node modules

--- index.js
+++ index.js
@@ -8,43 +8,24 @@ const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Middle ware to parse the json data
-app.use(express.json());``
-
-// Middleware to parse URL-encoded form data
-app.use(express.urlencoded({ extended: true }));
-
-// Set the view engine to ejs
+// Set the view engine to EJS
 app.set('view engine', 'ejs');
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.render('signUp');
     res.status(200);
 });
 
-app.post('/', (req, res) => {
 
-});
-
-app.get("/blogpost", (req, res) => {
-    
-});
-
-// Delete post path
-app.delete('/post/:id', (req, res) => {
-
-});
-
-// Edit post path
-app.put('/post/:id', (req, res) => {
-  
-});
-
+// Serve static files from the 'public' directory
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
